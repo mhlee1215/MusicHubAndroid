6e146b7191f6aa9da0bb41d5f6b8d1e43cad7407
mhlee <mhlee1215@gmail.com>
2016-03-24 14:39:14 -0700

    first commit

---
+++ app/src/main/java/android/musichub/com/musichubandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package android.musichub.com.musichubandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
